5de5791a056bad882400784d2daf8cc9e90a92ed
Vilina Osilova <vilina.osilova@jambit.com>
2024-05-27 14:32:24 +0400

    Adjust layout for max width

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,15 @@ import { SpeedInsights } from '@vercel/speed-insights/nuxt';
 </script>
 
 <template>
-  <div class="flex h-screen flex-col justify-between">
+  <div class="flex h-screen flex-col justify-between font-openSans">
     <Header />
-    <main class="m-auto min-h-full max-w-2xl">
+    <div
+      class="absolute left-0 top-18 z-30 flex w-full flex-col bg-white text-p1-semibold lg:relative lg:top-0 lg:flex-row lg:bg-none [&>*>*>li]:flex [&>*>*>li]:w-full [&>*>*>li]:justify-center [&>*>*]:flex [&>*>*]:flex-col"
+    >
+      <div id="nav" class="px-5 py-6 empty:p-0"></div>
+      <div id="nav2" class="px-5 pb-6 empty:p-0"></div>
+    </div>
+    <main class="min-h-full w-full max-w-screen-2xl self-center">
       <slot />
     </main>
     <Footer />
