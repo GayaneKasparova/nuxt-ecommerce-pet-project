dcc483b78fab16addf7a9b570281f61ed556e038
rbarseghyan <Ruben.Barseghyan@partner.bmw.de>
2024-05-30 12:49:50 +0400

    Add styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@ import { SpeedInsights } from '@vercel/speed-insights/nuxt';
   <div class="flex flex-col justify-between font-openSans">
     <Header />
     <div
-      class="top-18 absolute left-0 z-30 flex w-full flex-col bg-white text-p1-semibold lg:relative lg:top-0 lg:flex-row lg:bg-none [&>*>*>li]:flex [&>*>*>li]:w-full [&>*>*>li]:justify-center [&>*>*]:flex [&>*>*]:flex-col"
+      class="absolute left-0 top-18 z-30 flex w-full flex-col bg-white text-p1-semibold lg:relative lg:top-0 lg:flex-row lg:bg-none [&>*>*>li]:flex [&>*>*>li]:w-full [&>*>*>li]:justify-center [&>*>*]:flex [&>*>*]:flex-col"
     >
       <div id="nav" class="px-5 py-6 empty:p-0"></div>
       <div id="nav2" class="px-5 pb-6 empty:p-0"></div>
